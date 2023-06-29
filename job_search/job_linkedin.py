1b9ae0402d23eb9796800b1c3e85693f5c173bb9
BrianLoe <corneliusbrian.loe@students.mq.edu.au>
2023-06-29 22:10:50 +1000

    fix code in python script

--- job_search/job_linkedin.py
+++ job_search/job_linkedin.py
@@ -7,14 +7,31 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 import sys
+import openai
+params = config()
+openai.api_key = params['api_key']
 
-def search_job_in_page(job_list, cur_page_num):    
-    for row in job_list:
+def search_job_in_page(job_list, cur_page_num):
+    job_name = job_list.find_all('li', {'class':'ember-view'})
+    for row in job_name:
         try:
             job_title = row.find('a').contents[0].strip()
             company = row.find('div', {'class':'job-card-container__company-name'}).contents[0].strip()
+            level = job_list.find('li', {'class':'jobs-unified-top-card__job-insight'}).find('span').contents[2]
+            text = job_list.find('div', {'class':'jobs-description__content jobs-description-content'}).find('span').getText().strip()
+            system_msg="You are a career coach who understands job requirements"
+            user_msg="List 5 key skills to be a great fit in this job description "+text
+            response = openai.ChatCompletion.create(
+                                                    model="gpt-3.5-turbo",
+                                                    messages=[{"role": "system", "content": system_msg},
+                                                              {"role": "user", "content": user_msg}]
+                                                   )
             print('Job Title: ',job_title)
             print('Company: ', company)
+            print('Level: ',level)
+            print('5 Key Skills Needed according to GPT: ')
+            print(response["choices"][0]["message"]["content"])
+            print()
         except: 
             try:
                 p_num = int(row.find('button').attrs.get('aria-label')[-1:])
@@ -34,12 +51,12 @@ def get_job_titles_company(last_page):
         # Now using beautiful soup
         soup = BeautifulSoup(src, 'html5lib')
 
-        job_list = soup.find_all('li', {'class':'ember-view'})
-        print('Listing all job lists in page ', i)
+        job_list = soup.find('div', {'class':'scaffold-layout__list-detail-inner'})
+        print('||Listing all job lists in page ', str(i)+'||')
         next_page_num = search_job_in_page(job_list,i)
-        print('\n')
+        print()
         driver.find_element(By.XPATH, "//button[@aria-label='Page "+str(next_page_num)+"']").click()
-        time.sleep(5)
+        time.sleep(6)
     return next_page_num
 
 def input_rec_url():
@@ -48,7 +65,7 @@ def input_rec_url():
         print("Job site is invalid")
         ans = input("would you like to use existing link? [y/n]")
         if ans.lower()=='y':
-            recomend_url = "https://www.linkedin.com/jobs/collections/recommendedlipi=urn%3Ali%3Apage%3Ad_flagship3_job_home%3BobHSV5i9RA6yU%2F%2F5gLL1pQ%3D%3D"
+            recomend_url = "https://www.linkedin.com/jobs/collections/recommended?lipi=urn%3Ali%3Apage%3Ad_flagship3_job_home%3BobHSV5i9RA6yU%2F%2F5gLL1pQ%3D%3D"
         else:
             print("Exiting...")
             sys.exit(1)
@@ -71,7 +88,6 @@ def error_handler(input_arg):
         input_num_pages()
 
 if __name__ == "__main__":
-    params = config()
     recomend_url = input_rec_url()
             
     num_pages = int(input_num_pages())
@@ -106,14 +122,11 @@ if __name__ == "__main__":
     # Format (syntax) of writing XPath --> //tagname[@attribute='value']
     driver.find_element(By.XPATH, "//button[@type='submit']").click()
     # In case of an error, try changing the XPath used here.
-       
-    jobs_url = "https://www.linkedin.com/jobs/"
-    
-    print("Getting the jobs page ...")
-    driver.get(jobs_url)
     time.sleep(5)
+    
     print("Getting the recommended jobs page ...")
     driver.get(recomend_url)
     time.sleep(5)
+    
     print("Getting the job lists ...")
     get_job_titles_company(num_pages)
